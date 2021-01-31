8385b792139a310e686f8f5addf13e8bca0450b1
Helen Lazar <helen.lazar.77@gmail.com>
2021-01-31 22:30:44 +0100

    Add dataset SQL

---
+++ sql/populate.sql
@@ -0,0 +1,202 @@
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 1', 'Descr 1', 'Short descr 1', 24044, '1947-07-10 12:50:16');
+insert into movie_genre(genre_id, movie_id) values (1, 1);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 2', 'Descr 2', 'Short descr 2', 32302, '1980-06-10 12:50:16');
+insert into movie_genre(genre_id, movie_id) values (1, 2);
+insert into movie_genre(genre_id, movie_id) values (2, 2);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 3', 'Descr 3', 'Short descr 3', 10733, '1964-12-03 11:50:16');
+insert into movie_genre(genre_id, movie_id) values (1, 3);
+insert into movie_genre(genre_id, movie_id) values (2, 3);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 4', 'Descr 4', 'Short descr 4', 30056, '1952-07-19 11:50:16');
+insert into movie_genre(genre_id, movie_id) values (1, 4);
+insert into movie_genre(genre_id, movie_id) values (2, 4);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 5', 'Descr 5', 'Short descr 5', 20491, '1945-01-05 11:50:16');
+insert into movie_genre(genre_id, movie_id) values (1, 5);
+insert into movie_genre(genre_id, movie_id) values (2, 5);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 6', 'Descr 6', 'Short descr 6', 26909, '1947-11-12 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 6, '16398.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 6);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 7', 'Descr 7', 'Short descr 7', 5724, '1969-07-10 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 7, '24755.jpg');
+insert into movie_image(image_type_id, movie_id, url) values (2, 7, '3874.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 7);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 8', 'Descr 8', 'Short descr 8', 16128, '2020-11-09 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 8, '29447.jpg');
+insert into movie_image(image_type_id, movie_id, url) values (2, 8, '22488.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 8);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 9', 'Descr 9', 'Short descr 9', 10513, '1964-07-13 12:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 9, '13040.jpg');
+insert into movie_image(image_type_id, movie_id, url) values (2, 9, '26421.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 9);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 10', 'Descr 10', 'Short descr 10', 32462, '1962-09-29 12:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 10, '24033.jpg');
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 11', 'Descr 11', 'Short descr 11', 6970, '1971-05-14 11:50:16');
+insert into movie_genre(genre_id, movie_id) values (1, 11);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 12', 'Descr 12', 'Short descr 12', 31214, '1992-10-18 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 12, '31480.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 12);
+insert into movie_genre(genre_id, movie_id) values (2, 12);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 13', 'Descr 13', 'Short descr 13', 16439, '2011-08-30 12:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 13, '19021.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 13);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 14', 'Descr 14', 'Short descr 14', 22151, '2016-12-24 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 14, '27024.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 14);
+insert into movie_genre(genre_id, movie_id) values (2, 14);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 15', 'Descr 15', 'Short descr 15', 16708, '1949-01-24 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 15, '24560.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 15);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 16', 'Descr 16', 'Short descr 16', 5622, '1960-02-29 11:50:16');
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 17', 'Descr 17', 'Short descr 17', 5921, '1988-06-29 12:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 17, '24606.jpg');
+insert into movie_image(image_type_id, movie_id, url) values (2, 17, '27814.jpg');
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 18', 'Descr 18', 'Short descr 18', 19530, '1978-01-12 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 18, '4617.jpg');
+insert into movie_image(image_type_id, movie_id, url) values (2, 18, '4162.jpg');
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 19', 'Descr 19', 'Short descr 19', 24374, '1979-03-10 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 19, '4703.jpg');
+insert into movie_image(image_type_id, movie_id, url) values (2, 19, '16278.jpg');
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 20', 'Descr 20', 'Short descr 20', 26056, '1948-02-18 11:50:16');
+insert into movie_genre(genre_id, movie_id) values (1, 20);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 21', 'Descr 21', 'Short descr 21', 30451, '2020-09-29 12:50:16');
+insert into movie_genre(genre_id, movie_id) values (1, 21);
+insert into movie_genre(genre_id, movie_id) values (2, 21);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 22', 'Descr 22', 'Short descr 22', 7820, '1991-05-01 12:50:16');
+insert into movie_genre(genre_id, movie_id) values (1, 22);
+insert into movie_genre(genre_id, movie_id) values (2, 22);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 23', 'Descr 23', 'Short descr 23', 16796, '1983-06-12 12:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 23, '21635.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 23);
+insert into movie_genre(genre_id, movie_id) values (2, 23);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 24', 'Descr 24', 'Short descr 24', 22545, '2004-10-16 12:50:16');
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 25', 'Descr 25', 'Short descr 25', 6754, '2016-10-28 12:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 25, '29278.jpg');
+insert into movie_image(image_type_id, movie_id, url) values (2, 25, '12082.jpg');
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 26', 'Descr 26', 'Short descr 26', 10812, '2020-06-15 12:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 26, '7957.jpg');
+insert into movie_image(image_type_id, movie_id, url) values (2, 26, '20092.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 26);
+insert into movie_genre(genre_id, movie_id) values (2, 26);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 27', 'Descr 27', 'Short descr 27', 16639, '1989-11-13 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 27, '11652.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 27);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 28', 'Descr 28', 'Short descr 28', 22480, '1956-07-18 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 28, '2512.jpg');
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 29', 'Descr 29', 'Short descr 29', 18951, '2011-04-13 12:50:16');
+insert into movie_genre(genre_id, movie_id) values (1, 29);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 30', 'Descr 30', 'Short descr 30', 14759, '1937-06-18 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 30, '17831.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 30);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 31', 'Descr 31', 'Short descr 31', 1755, '1956-08-17 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 31, '31378.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 31);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 32', 'Descr 32', 'Short descr 32', 9574, '1949-12-30 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 32, '23686.jpg');
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 33', 'Descr 33', 'Short descr 33', 316, '1936-09-25 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 33, '9950.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 33);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 34', 'Descr 34', 'Short descr 34', 15894, '2003-12-16 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 34, '26178.jpg');
+insert into movie_image(image_type_id, movie_id, url) values (2, 34, '2058.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 34);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 35', 'Descr 35', 'Short descr 35', 22738, '1940-03-13 11:50:16');
+insert into movie_genre(genre_id, movie_id) values (1, 35);
+insert into movie_genre(genre_id, movie_id) values (2, 35);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 36', 'Descr 36', 'Short descr 36', 1014, '1973-07-27 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 36, '30505.jpg');
+insert into movie_image(image_type_id, movie_id, url) values (2, 36, '14827.jpg');
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 37', 'Descr 37', 'Short descr 37', 25354, '1984-01-30 11:50:16');
+insert into movie_genre(genre_id, movie_id) values (1, 37);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 38', 'Descr 38', 'Short descr 38', 23040, '2017-10-18 12:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 38, '3281.jpg');
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 39', 'Descr 39', 'Short descr 39', 20753, '1951-07-25 11:50:16');
+insert into movie_genre(genre_id, movie_id) values (1, 39);
+insert into movie_genre(genre_id, movie_id) values (2, 39);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 40', 'Descr 40', 'Short descr 40', 16130, '1938-02-21 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 40, '21249.jpg');
+insert into movie_image(image_type_id, movie_id, url) values (2, 40, '11635.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 40);
+insert into movie_genre(genre_id, movie_id) values (2, 40);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 41', 'Descr 41', 'Short descr 41', 28305, '1950-05-11 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 41, '3454.jpg');
+insert into movie_image(image_type_id, movie_id, url) values (2, 41, '5103.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 41);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 42', 'Descr 42', 'Short descr 42', 21670, '1978-03-16 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 42, '413.jpg');
+insert into movie_image(image_type_id, movie_id, url) values (2, 42, '10828.jpg');
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 43', 'Descr 43', 'Short descr 43', 17548, '1971-07-29 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 43, '13943.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 43);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 44', 'Descr 44', 'Short descr 44', 23822, '1990-06-06 12:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 44, '15335.jpg');
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 45', 'Descr 45', 'Short descr 45', 1622, '1986-11-22 11:50:16');
+insert into movie_genre(genre_id, movie_id) values (1, 45);
+insert into movie_genre(genre_id, movie_id) values (2, 45);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 46', 'Descr 46', 'Short descr 46', 25375, '1982-08-15 12:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 46, '2067.jpg');
+insert into movie_image(image_type_id, movie_id, url) values (2, 46, '18549.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 46);
+insert into movie_genre(genre_id, movie_id) values (2, 46);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 47', 'Descr 47', 'Short descr 47', 30512, '1967-10-24 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 47, '18259.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 47);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 48', 'Descr 48', 'Short descr 48', 22988, '1978-01-05 11:50:16');
+insert into movie_genre(genre_id, movie_id) values (1, 48);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 49', 'Descr 49', 'Short descr 49', 10366, '1970-10-09 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 49, '17857.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 49);
+insert into movie_genre(genre_id, movie_id) values (2, 49);
+
+insert into movie(title, description, short_description, duration, release_date) values ('My Movie 50', 'Descr 50', 'Short descr 50', 23536, '1966-10-23 11:50:16');
+insert into movie_image(image_type_id, movie_id, url) values (1, 50, '19044.jpg');
+insert into movie_genre(genre_id, movie_id) values (1, 50);
+insert into movie_genre(genre_id, movie_id) values (2, 50);
+
